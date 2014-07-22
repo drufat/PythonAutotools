ecb639d623b6b228d5ea45ed2e42ef0a9344fc91
Dzhelil Rufat <drufat@caltech.edu>
2014-07-23 01:14:51 +0200

    Delete completely instead of moving to trash.

--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rmtrash Makefile Makefile.in aclocal.m4 ar-lib autom4te.cache depcomp compile config.* configure *.o *.lo *.la install-sh libtool ltmain.sh m4 missing stamp-h1 .deps .libs
+rm -rf Makefile Makefile.in aclocal.m4 ar-lib autom4te.cache depcomp compile config.* configure *.o *.lo *.la install-sh libtool ltmain.sh m4 missing stamp-h1 .deps .libs
