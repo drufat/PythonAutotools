570715521094310d5fc2500dadac8ba3bde730d4
Dzhelil Rufat <drufat@caltech.edu>
2015-04-30 00:34:19 -0700

    clean build

--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rm -rf Makefile Makefile.in aclocal.m4 ar-lib autom4te.cache depcomp compile config.* configure *.o *.lo *.la *.so install-sh libtool ltmain.sh m4 missing stamp-h1 .deps .libs
+rm -rf Makefile Makefile.in aclocal.m4 ar-lib autom4te.cache depcomp compile config.* configure *.o *.lo *.la *.so install-sh libtool ltmain.sh m4 missing stamp-h1 .deps .libs build
